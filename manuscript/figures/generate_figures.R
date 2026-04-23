c4d50f4be5c9f1e674e3e7225931795aa3381b77
PietroCarlotti <91976479+PietroCarlotti@users.noreply.github.com>
2026-04-23 14:33:11 -0500

    Create generate_figures.R

---
+++ manuscript/figures/generate_figures.R
@@ -0,0 +1,41 @@
+# Load necessary libraries
+library(ggplot2)
+library(BSET)
+
+# Clear console and environment
+rm(list = ls())
+graphics.off()
+cat("\014")
+
+# Set working directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#######################
+# Discrepancy heatmap #
+#######################
+
+
+
+#####################################
+# DCCT primary outcome distribution #
+#####################################
+
+
+
+###############################
+# DCCT surrogate distribution #
+###############################
+
+
+
+#####################################
+# DCCT posterior without covariates #
+#####################################
+
+
+
+##################################
+# DCCT posterior with covariates #
+##################################
+
+
